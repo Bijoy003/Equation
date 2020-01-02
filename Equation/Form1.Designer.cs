1107f16d4de1d6810e3c05e30ea1b1caf083342e
Rasel Hossain <raselhossain.pstu@gmail.com>
2020-01-03 03:30:31 +0600

    Animation Done

--- Equation/Form1.Designer.cs
+++ Equation/Form1.Designer.cs
@@ -29,30 +29,23 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(722, 390);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(188, 119);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // pictureBox3
             // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Default;
             this.pictureBox3.Image = global::Equation.Properties.Resources.Ball;
-            this.pictureBox3.Location = new System.Drawing.Point(387, 72);
+            this.pictureBox3.Location = new System.Drawing.Point(125, 72);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(65, 62);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -61,6 +54,7 @@
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
             this.pictureBox2.Image = global::Equation.Properties.Resources.black;
             this.pictureBox2.Location = new System.Drawing.Point(105, 210);
             this.pictureBox2.Name = "pictureBox2";
@@ -72,26 +66,44 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Equation.Properties.Resources.RHBBC;
-            this.pictureBox1.Location = new System.Drawing.Point(105, 210);
+            this.pictureBox1.Location = new System.Drawing.Point(153, 210);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(715, 112);
+            this.pictureBox1.Size = new System.Drawing.Size(611, 90);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // timer1
             // 
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 140;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Emoji", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(383, 367);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 28);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Loading 0%";
             // 
             // Loading
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(960, 540);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Loading";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -101,16 +113,17 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Equation/Form1.cs
+++ Equation/Form1.cs
@@ -17,25 +17,89 @@ namespace Equation
         public Loading()
         {
             InitializeComponent();
+            timer1.Start();
+            timer2.Start();
         }
 
+        /*
         private void button1_Click(object sender, EventArgs e)
         {
             var f = new MainMenu(dif,mod);
             this.Hide();
             f.Show();
-        }
+        }*/
 
         private void Loading_Load(object sender, EventArgs e)
         {
 
         }
 
-        int direction = 0;
+        int direction = 0 , sp = 1;
+
+        int loading = 0 , cnt = 1;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (loading == 58 && cnt<6)
+            {
+                cnt++;
+            }
+            else
+            {
+                if (loading >= 100)
+                {
+                    if (loading > 100) loading = 100;
+                    if (cnt < 6)
+                    {
+                        label1.Text = "Loading " + loading.ToString() + "%";
+                        cnt++;
+                    }
+                    else
+                    {
+                        var f = new MainMenu(dif, mod);
+                        this.Hide();
+                        f.Show();
+                    }
+                }
+                else
+                {
+                    cnt = 0;
+                    loading = loading + 1;
+                    if (loading > 58) loading++;
+                    label1.Text = "Loading " + loading.ToString() + "%";
+                }
+            }
+        }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            int diff = (int)(pictureBox2.Location.Y - pictureBox3.Location.Y) - 58;
+            diff = 140 - diff;
+            
+            if (diff < 0) diff = 0;
 
+            sp = ((int)diff / 14) + 1;
+            if (direction == 0)
+            {
+                pictureBox3.Location = new Point(pictureBox3.Location.X, pictureBox3.Location.Y + sp);
+                if (pictureBox2.Location.Y - pictureBox3.Location.Y < 58)
+                {
+                    if (pictureBox2.Location.Y < 304) {
+                        pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y + sp);
+                        pictureBox3.Location = new Point(pictureBox3.Location.X, pictureBox3.Location.Y + sp);
+                    }
+
+                    direction = 1 - direction;
+                }
+            }
+            else
+            {
+                pictureBox3.Location = new Point(pictureBox3.Location.X, pictureBox3.Location.Y - sp);
+                if (pictureBox2.Location.Y - pictureBox3.Location.Y >198)
+                {
+                    direction = 1 - direction;
+                }
+            }
         }
     }
 }
