1208f16204de1cd1e6e683e8f831c6f27c9fd0dc
Rasel Hossain <raselhossain.pstu@gmail.com>
2020-01-04 00:42:31 +0600

    StartVariable complete.

--- Equation/StartVariable.Designer.cs
+++ Equation/StartVariable.Designer.cs
@@ -28,77 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.Exit = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.Done = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.thirdVariable = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.Score = new System.Windows.Forms.Label();
-            this.Time = new System.Windows.Forms.Label();
             this.Result = new System.Windows.Forms.Label();
-            this.ResultVariable = new System.Windows.Forms.Label();
+            this.operation2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.SecondVariable = new System.Windows.Forms.Label();
-            this.FirstVariable = new System.Windows.Forms.Label();
+            this.operation1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.inform = new System.Windows.Forms.Label();
+            this.Done = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.firstVariable = new System.Windows.Forms.TextBox();
+            this.secondVariable = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // Exit
+            // thirdVariable
             // 
-            this.Exit.Location = new System.Drawing.Point(134, 324);
-            this.Exit.Name = "Exit";
-            this.Exit.Size = new System.Drawing.Size(75, 23);
-            this.Exit.TabIndex = 9;
-            this.Exit.Text = "Exit";
-            this.Exit.UseVisualStyleBackColor = true;
-            this.Exit.Click += new System.EventHandler(this.Exit_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(134, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 8;
-            this.button4.Text = "Back";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // Done
-            // 
-            this.Done.Location = new System.Drawing.Point(337, 401);
-            this.Done.Name = "Done";
-            this.Done.Size = new System.Drawing.Size(270, 45);
-            this.Done.TabIndex = 21;
-            this.Done.Text = "Done";
-            this.Done.UseVisualStyleBackColor = true;
-            this.Done.Click += new System.EventHandler(this.Done_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(469, 284);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(37, 44);
-            this.textBox2.TabIndex = 20;
-            this.textBox2.Text = "+";
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(316, 284);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(37, 44);
-            this.textBox1.TabIndex = 10;
-            this.textBox1.Text = "*";
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.thirdVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdVariable.Location = new System.Drawing.Point(554, 244);
+            this.thirdVariable.Name = "thirdVariable";
+            this.thirdVariable.Size = new System.Drawing.Size(44, 44);
+            this.thirdVariable.TabIndex = 20;
+            this.thirdVariable.Text = "21";
+            this.thirdVariable.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.label2.Location = new System.Drawing.Point(380, 175);
+            this.label2.Location = new System.Drawing.Point(371, 133);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(183, 55);
             this.label2.TabIndex = 11;
@@ -110,112 +72,153 @@
             this.Score.AutoSize = true;
             this.Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Score.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.Score.Location = new System.Drawing.Point(529, 175);
+            this.Score.Location = new System.Drawing.Point(546, 133);
             this.Score.Name = "Score";
             this.Score.Size = new System.Drawing.Size(52, 55);
             this.Score.TabIndex = 13;
-            this.Score.Text = "4";
-            // 
-            // Time
-            // 
-            this.Time.AutoSize = true;
-            this.Time.Font = new System.Drawing.Font("Microsoft Sans Serif", 54.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Time.Location = new System.Drawing.Point(454, 49);
-            this.Time.Name = "Time";
-            this.Time.Size = new System.Drawing.Size(77, 83);
-            this.Time.TabIndex = 14;
-            this.Time.Text = "4";
+            this.Score.Text = "0";
             // 
             // Result
             // 
             this.Result.AutoSize = true;
             this.Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Result.Location = new System.Drawing.Point(664, 288);
+            this.Result.Location = new System.Drawing.Point(683, 248);
             this.Result.Name = "Result";
             this.Result.Size = new System.Drawing.Size(36, 37);
             this.Result.TabIndex = 15;
             this.Result.Text = "4";
             // 
-            // ResultVariable
+            // operation2
             // 
-            this.ResultVariable.AutoSize = true;
-            this.ResultVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ResultVariable.Location = new System.Drawing.Point(542, 288);
-            this.ResultVariable.Name = "ResultVariable";
-            this.ResultVariable.Size = new System.Drawing.Size(36, 37);
-            this.ResultVariable.TabIndex = 16;
-            this.ResultVariable.Text = "3";
+            this.operation2.AutoSize = true;
+            this.operation2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operation2.Location = new System.Drawing.Point(492, 248);
+            this.operation2.Name = "operation2";
+            this.operation2.Size = new System.Drawing.Size(27, 37);
+            this.operation2.TabIndex = 16;
+            this.operation2.Text = "/";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(608, 288);
+            this.label4.Location = new System.Drawing.Point(626, 248);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(37, 37);
             this.label4.TabIndex = 17;
             this.label4.Text = "=";
             // 
-            // SecondVariable
+            // operation1
             // 
-            this.SecondVariable.AutoSize = true;
-            this.SecondVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SecondVariable.Location = new System.Drawing.Point(398, 288);
-            this.SecondVariable.Name = "SecondVariable";
-            this.SecondVariable.Size = new System.Drawing.Size(36, 37);
-            this.SecondVariable.TabIndex = 18;
-            this.SecondVariable.Text = "2";
+            this.operation1.AutoSize = true;
+            this.operation1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.operation1.Location = new System.Drawing.Point(335, 248);
+            this.operation1.Name = "operation1";
+            this.operation1.Size = new System.Drawing.Size(37, 37);
+            this.operation1.TabIndex = 18;
+            this.operation1.Text = "+";
             // 
-            // FirstVariable
+            // pictureBox1
             // 
-            this.FirstVariable.AutoSize = true;
-            this.FirstVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FirstVariable.Location = new System.Drawing.Point(244, 288);
-            this.FirstVariable.Name = "FirstVariable";
-            this.FirstVariable.Size = new System.Drawing.Size(34, 37);
-            this.FirstVariable.TabIndex = 19;
-            this.FirstVariable.Text = "1";
+            this.pictureBox1.Image = global::Equation.Properties.Resources.TimeBar2;
+            this.pictureBox1.Location = new System.Drawing.Point(-932, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(934, 56);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // inform
+            // 
+            this.inform.AutoSize = true;
+            this.inform.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inform.Location = new System.Drawing.Point(392, 327);
+            this.inform.Name = "inform";
+            this.inform.Size = new System.Drawing.Size(0, 24);
+            this.inform.TabIndex = 23;
+            // 
+            // Done
+            // 
+            this.Done.FlatAppearance.BorderColor = System.Drawing.Color.LimeGreen;
+            this.Done.FlatAppearance.BorderSize = 3;
+            this.Done.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Done.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Done.Location = new System.Drawing.Point(381, 373);
+            this.Done.Name = "Done";
+            this.Done.Size = new System.Drawing.Size(215, 76);
+            this.Done.TabIndex = 24;
+            this.Done.Text = "Done";
+            this.Done.UseVisualStyleBackColor = true;
+            this.Done.Click += new System.EventHandler(this.Done_Click_1);
+            this.Done.MouseEnter += new System.EventHandler(this.Done_MouseEnter);
+            this.Done.MouseLeave += new System.EventHandler(this.Done_MouseLeave);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // firstVariable
+            // 
+            this.firstVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstVariable.Location = new System.Drawing.Point(253, 244);
+            this.firstVariable.Name = "firstVariable";
+            this.firstVariable.Size = new System.Drawing.Size(45, 44);
+            this.firstVariable.TabIndex = 10;
+            this.firstVariable.Text = "19";
+            this.firstVariable.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // secondVariable
+            // 
+            this.secondVariable.AutoSize = true;
+            this.secondVariable.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondVariable.Location = new System.Drawing.Point(404, 248);
+            this.secondVariable.Name = "secondVariable";
+            this.secondVariable.Size = new System.Drawing.Size(55, 37);
+            this.secondVariable.TabIndex = 18;
+            this.secondVariable.Text = "43";
             // 
             // StartVariable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(944, 501);
             this.Controls.Add(this.Done);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.inform);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.thirdVariable);
+            this.Controls.Add(this.firstVariable);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Score);
-            this.Controls.Add(this.Time);
             this.Controls.Add(this.Result);
-            this.Controls.Add(this.ResultVariable);
+            this.Controls.Add(this.operation2);
+            this.Controls.Add(this.secondVariable);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.SecondVariable);
-            this.Controls.Add(this.FirstVariable);
-            this.Controls.Add(this.Exit);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.operation1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "StartVariable";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StartVariable";
+            this.Load += new System.EventHandler(this.StartVariable_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button Exit;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button Done;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox thirdVariable;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label Score;
-        private System.Windows.Forms.Label Time;
         private System.Windows.Forms.Label Result;
-        private System.Windows.Forms.Label ResultVariable;
+        private System.Windows.Forms.Label operation2;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label SecondVariable;
-        private System.Windows.Forms.Label FirstVariable;
+        private System.Windows.Forms.Label operation1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label inform;
+        private System.Windows.Forms.Button Done;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox firstVariable;
+        private System.Windows.Forms.Label secondVariable;
     }
 }
--- Equation/StartVariable.cs
+++ Equation/StartVariable.cs
@@ -12,39 +12,91 @@ namespace Equation
 {
     public partial class StartVariable : Form
     {
-        public int dif, mod;
-        int a = 28, b = 5, c = 3, op1 = 4, op2 = 4, res;
         Random rand = new Random();
-        public StartVariable(int di,int mo)
+        public StartVariable(int di, int mo)
         {
             InitializeComponent();
             dif = di;
             mod = mo;
+            timer1.Start();
+            setEquation();
         }
 
-        private void button4_Click(object sender, EventArgs e)
+        int dif, mod,speed = 2;
+        int a = 28, b = 5, c = 3, op1 = 4, op2 = 4, res;
+
+        private void StartVariable_Load(object sender, EventArgs e)
         {
-            var f = new MainMenu(dif, mod);
-            this.Hide();
-            f.Show();
+
         }
 
-        private void Exit_Click(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            Environment.Exit(0);
+            pictureBox1.Location = new Point(pictureBox1.Location.X + speed, pictureBox1.Location.Y);
+            if (pictureBox1.Location.X > -12)
+            {
+                timer1.Stop();
+                GameOver g = new GameOver(dif, mod, Int32.Parse(Score.Text));
+                this.Hide();
+                g.Show();
+            }
         }
 
-        private void Done_Click(object sender, EventArgs e)
+        private void Done_MouseLeave(object sender, EventArgs e)
         {
+            Done.FlatAppearance.BorderSize = 3;
+            Done.BackColor = Color.White;
+            Done.FlatAppearance.BorderColor = Color.Green;
+        }
 
+        private void Done_MouseEnter(object sender, EventArgs e)
+        {
+            Done.FlatAppearance.BorderSize = 0;
+            Done.BackColor = Color.SkyBlue;
+        }
+
+        private void Done_Click_1(object sender, EventArgs e)
+        {
+            int _aa = Int32.Parse(firstVariable.Text);
+            int _cc = Int32.Parse(thirdVariable.Text);
+           
+            /*if ((s1.Length > 1) || (s2.Length > 1) || (s1 != "+" && s1 != "-" && s1 != "*" && s1 != "/") || (s2 != "+" && s2 != "-" && s2 != "*" && s2 != "/"))
+            {
+                inform.Text = "Invalid operator";
+                inform.ForeColor = Color.OrangeRed;
+            }
+            */
+            if (res == CalculateResult(_aa, op1, b, op2, _cc))
+            {
+                //score count
+                int sc = Int32.Parse(Score.Text);
+                sc++;
+                Score.Text = sc.ToString();
+                //
+
+                //Refresh Time
+                speed += 2;
+                pictureBox1.Location = new Point(-932, 44);
+                //
+
+                setEquation();
+            }
+            else
+            {
+                inform.Text = "Wrong answer, Try Again";
+                inform.ForeColor = Color.Red;
+            }
         }
 
         void setEquation()
         {
             //initialize the values here
 
-            a = rand.Next(1, 20);
-            MessageBox.Show(a.ToString());
+            a = rand.Next(1, 10);
+            b = rand.Next(1, 10);
+            c = rand.Next(1, 10);
+            op1 = rand.Next(1, 5);
+            op2 = rand.Next(1, 5);
 
             //
 
@@ -68,15 +120,129 @@ namespace Equation
                 //MessageBox.Show(b.ToString());
                 //MessageBox.Show(c.ToString());
 
-                res = a / b / c;
+                res = CalculateResult(a, op1, b, op2, c);
 
                 //MessageBox.Show(res.ToString());
             }
             else
             {
-                int firstPart = 0, secondParet = 0;
-                if (op1 == 4)
+                res = CalculateResult(a, op1, b, op2, c);
+            }
+
+            string FindOperator(int _a)
+            {
+                string _s = "+";
+                if (_a == 2) _s = "-";
+                if (_a == 3) _s = "*";
+                if (_a == 4) _s = "/";
+                return _s;
+            }
+
+            secondVariable.Text = b.ToString();
+            Result.Text = res.ToString();
+            operation1.Text = FindOperator(op1);
+            operation2.Text = FindOperator(op2);
+            firstVariable.Text = "";
+            thirdVariable.Text = "";
+
+        }
+
+        int CalculateResult(int aa, int oop1, int bb, int oop2, int cc)
+        {
+            int firstPart = 0, secondParet = 0, rres = 0;
+            if (oop1 == 4)
+            {
+                firstPart = aa / bb;
+                if (oop2 == 4)
                 {
+                    rres = firstPart / cc;
+                }
+                if (oop2 == 3)
+                {
+                    rres = firstPart * cc;
+                }
+                if (oop2 == 2)
+                {
+                    rres = firstPart - cc;
+                }
+                if (oop2 == 1)
+                {
+                    rres = firstPart + cc;
+                }
+            }
+            else if (oop1 == 3)
+            {
+                firstPart = aa * bb;
+                if (oop2 == 4)
+                {
+                    rres = firstPart / cc;
+                }
+                if (oop2 == 3)
+                {
+                    rres = firstPart * cc;
+                }
+                if (oop2 == 2)
+                {
+                    rres = firstPart - cc;
+                }
+                if (oop2 == 1)
+                {
+                    rres = firstPart + cc;
+                }
+            }
+            else if (oop2 == 4)
+            {
+                secondParet = bb / cc;
+                if (oop1 == 4)
+                {
+                    rres = aa / secondParet;
+                }
+                if (oop1 == 3)
+                {
+                    rres = aa * secondParet;
+                }
+                if (oop1 == 2)
+                {
+                    rres = aa - secondParet;
+                }
+                if (oop1 == 1)
+                {
+                    rres = aa + secondParet;
+                }
+            }
+            else if (oop2 == 3)
+            {
+                secondParet = bb * cc;
+                if (oop1 == 4)
+                {
+                    rres = aa / secondParet;
+                }
+                if (oop1 == 3)
+                {
+                    rres = aa * secondParet;
+                }
+                if (oop1 == 2)
+                {
+                    rres = aa - secondParet;
+                }
+                if (oop1 == 1)
+                {
+                    rres = aa + secondParet;
+                }
+            }
+            else
+            {
+                if (oop1 == 1)
+                {
+                    firstPart = aa + bb;
+                    if (oop2 == 1) rres = firstPart + cc;
+                    else rres = firstPart - cc;
+                }
+                if (oop1 == 2)
+                {
+                    firstPart = aa - bb;
+                    if (oop2 == 1) rres = firstPart + cc;
+                    else rres = firstPart - cc;
                     firstPart = a / b;
                     if (op2 == 4)
                     {
@@ -138,6 +304,8 @@ namespace Equation
 
             }
 
+            return rres;
         }
+
     }
 }
